ada305d54a4e87cae7dbf2068c05d3563079c210
Anirudha Kashid <91369967+Anirudhakashid@users.noreply.github.com>
2025-06-28 20:23:34 +0530

    Time: 0 ms (100%), Space: 42.8 MB (22.43%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+          int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+
+           int center = start + (end - start)/2;
+
+            if(arr[center] > arr[center + 1] ){
+                //we are in des part
+                end = center;
+            }else{
+                //we are in asc part
+                start = center + 1;
+            }
+        }    
+        return start; // can return end aswell as both are same
+    }
+}
