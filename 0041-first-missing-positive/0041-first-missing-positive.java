3afe92492b0487d4e5b66d2e4dd2d575dd56de4b
Anirudha Kashid <91369967+Anirudhakashid@users.noreply.github.com>
2025-07-12 13:36:14 +0530

    Time: 1 ms (100%), Space: 57.8 MB (23.24%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while(i<nums.length){
+            int correctIndex = nums[i] - 1;
+            //1st: ignore the negatives
+            //2nd: ignore nos > than arr length
+            if( nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correctIndex]){
+                //swaping
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }else{
+                i++;
+            }
+        }
+        for(int j=0; j<nums.length; j++){
+            if(nums[j] != j+1){
+                return j+1;
+            }
+        }
+        return nums.length + 1;
+    }
+}
