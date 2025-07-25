71b1e891ba8a3a6f3d8507ca148b981e5bcf02cf
Anirudha Kashid <91369967+Anirudhakashid@users.noreply.github.com>
2025-07-25 20:36:15 +0530

    Time: 1 ms (97.33%), Space: 43.5 MB (44.87%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+    //Using Integer instead of int so they can be set to null
+    //skip special-case logic for Integer.MIN_VALUE
+    //we are using min_value as test cases are in -100000
+        long max = Long.MIN_VALUE;
+        long second_max = Long.MIN_VALUE;
+        long third_max = Long.MIN_VALUE;
+
+        for(int num : nums){
+            //Handling dublicates
+            //if found continue to next element
+            if(num == max || num == second_max || num == third_max){
+                continue;
+            }
+
+            if(num > max){
+            //num is greater than current max → shift all values(3) down.
+                third_max = second_max;
+                second_max = max;
+                max = num;
+            }else if( num > second_max){
+            //num is greater than second max → shift all values(2) down.
+                third_max = second_max;
+                second_max = num;
+            }else if( num > third_max){
+                third_max = num;
+            }
+        }
+
+    //if 3rd max does not exists return the maximum element (as given in question)
+        if(third_max == Long.MIN_VALUE){
+            return (int) max;
+        }
+     // long -> int   
+        return (int) third_max;
+    }
+}
