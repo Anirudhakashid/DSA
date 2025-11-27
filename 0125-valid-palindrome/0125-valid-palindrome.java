e715c15ff665a4cb06f160984cac27fd959e597b
Anirudha Kashid <91369967+Anirudhakashid@users.noreply.github.com>
2025-11-27 13:04:00 +0530

    Time: 2 ms (98.68%), Space: 44 MB (62.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,37 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //Method 1:
-        StringBuilder newStr = new StringBuilder();
-        for(char c: s.toCharArray()){
-            if(Character.isLetterOrDigit(c)){
-                newStr.append(Character.toLowerCase(c));
-            }
+     //Method 1:
+        // StringBuilder newStr = new StringBuilder();
+        // for(char c: s.toCharArray()){
+        //     if(Character.isLetterOrDigit(c)){
+        //         newStr.append(Character.toLowerCase(c));
+        //     }
+        // }
+        // return newStr.toString().equals(newStr.reverse().toString());
+
+    //Method 2:Two pointer
+    int l = 0;
+    int r = s.length() - 1;
+
+    while(l < r){
+        while(l < r && !isAlphaNum(s.charAt(l))){
+            l++;
         }
-        return newStr.toString().equals(newStr.reverse().toString());
+
+        while(r > l && !isAlphaNum(s.charAt(r))){
+            r--;
+        }
+
+        if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+            return false;
+        }
+        l++; r--;
+    }
+    return true;
+}
+    public boolean isAlphaNum(char c){
+        return (c >= 'A' && c<= 'Z' ||
+                c >= 'a' && c<= 'z'||
+                c >= '0' && c<= '9');
     }
 }
